eeafb2b28c83b2d647369d125708144b2e78a400
smic <susanna@qburst.com>
2021-12-28 21:19:18 +0530

    Peak finding

--- algorithms/src/PeakFinding.java
+++ algorithms/src/PeakFinding.java
@@ -2,32 +2,32 @@ import java.util.Scanner;
 
 /**
  * Find a peak (position in the 0 indexed array) in the input array of numbers.
- *
+ * <p>
  * Peak - Any number is a peak if the numbers before and after are <= the number
  */
 
 public class PeakFinding {
 
-    public static void main(String [] a) {
+    public static void main(String[] a) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter array size");
         int n = scan.nextInt();
         scan.nextLine();
-        int [] input = new int [n];
+        int[] input = new int[n];
         System.out.println("Enter array. Type each number and press enter");
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             input[i] = scan.nextInt();
         }
         System.out.println(findPeak(input, 0, input.length - 1));
 
     }
 
-    public static int findPeak(int [] input, int i, int j) {
+    public static int findPeak(int[] input, int i, int j) {
         int n = j - i;
-        int mid = i + n/2;
-        if(mid > 0 && input[mid] < input[mid - 1]) {
+        int mid = i + n / 2;
+        if (mid > 0 && input[mid] < input[mid - 1]) {
             return findPeak(input, i, mid - 1);
-        } else if(mid < j && input[mid] < input[mid + 1]) {
+        } else if (mid < j && input[mid] < input[mid + 1]) {
             return findPeak(input, mid + 1, j);
         } else {
             return mid;
--- algorithms/src/PeakFinding2D.java
+++ algorithms/src/PeakFinding2D.java
@@ -2,36 +2,35 @@ import java.util.Scanner;
 
 /**
  * Find the peak (position in the 0 indexed array) in a 2D array
- *
+ * <p>
  * Peak - A number at a position (i, j) in array a is a peak iff a(i - 1, j) <= a(i, j) && a(i + 1, j) <= a(i, j)
  * && a(i, j + 1) <= a(i, j) && a(i, j - 1) <= a(i, j)
- *
  */
 public class PeakFinding2D {
 
-    public static void main(String [] a) {
+    public static void main(String[] a) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number of rows");
         int r = scan.nextInt();
         System.out.println("Enter the number of columns");
         int c = scan.nextInt();
         System.out.println("Enter the array elements");
-        int [][] input = new int [r][c];
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
+        int[][] input = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 input[i][j] = scan.nextInt();
             }
         }
         System.out.println(findPeak(input, 0, input.length - 1));
     }
 
-    public static String findPeak(int [][] input, int ir, int jr) {
+    public static String findPeak(int[][] input, int ir, int jr) {
         int diff = jr - ir;
         int mr = ir + diff;
         int max = findMax(input[mr]);
-        if(mr > 0 && input[mr-1][max] > input[mr][max]) {
+        if (mr > 0 && input[mr - 1][max] > input[mr][max]) {
             return findPeak(input, ir, mr - 1);
-        } else if(mr < jr && input[mr + 1][max] > input[mr][max]) {
+        } else if (mr < jr && input[mr + 1][max] > input[mr][max]) {
             return findPeak(input, mr + 1, jr);
         } else {
             return "(" + mr + ", " + max + ")";
@@ -40,8 +39,8 @@ public class PeakFinding2D {
 
     public static int findMax(int[] row) {
         int max = 0;
-        for(int i = 0; i < row.length; i++) {
-            if(row[i] > row[max]) {
+        for (int i = 0; i < row.length; i++) {
+            if (row[i] > row[max]) {
                 max = i;
             }
         }
